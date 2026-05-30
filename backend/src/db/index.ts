343af6b8f347185b35cb3fe41e2b805699832fd1
fauparada <fauparada@alumnos.uai.cl>
2026-05-30 12:05:21 -0400

    Corriegiendo algunos errores

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -3,14 +3,22 @@ import { Client } from 'pg';
 import * as schema from './schema';
 
 //Validamos que exista la variable de entorno
-if (!process.env.DATABASE_URL) {
-    throw new Error('La variable de entorno DATABASE_URL no está configurada');
-}
+//if (!process.env.DATABASE_URL) {
+    //throw new Error('La variable de entorno DATABASE_URL no está configurada');
+//}
+
+//Si no encuentra la variable, usamos la ruta local por defecto para que no se caiga
+const connectionString = process.env.DATABASE_URL || "postgresql://faustinaparada:@localhost:5432/edificio_reservas";
+
+//Pasamos esa ruta al cliente de Postgres
+export const client = new Client({
+  connectionString: connectionString,
+});
 
 //Configuramos el cliente nativo de Postgres
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-});
+//const client = new Client({
+    //connectionString: process.env.DATABASE_URL,
+//});
 
 //Nos conectamos de forma asíncrona
 await client.connect();
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'drizzle-kit';
 
 if (!process.env.DATABASE_URL) {
-    throw new Error('La variable de entorno DATABASE_URL no está configurada');
-}
+    throw new Error('DATABASE_URL no está configurada en las variables de entorno');
+  }
+
+// Le damos una URL de respaldo para que compile
+const dbUrl = process.env.DATABASE_URL || "postgresql://faustinaparada:@localhost:4005/edificio_reservas";
 
 export default defineConfig({
-    schema: './backend/src/db/schema.ts',
-    out: './drizzle',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL,
-    },
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: dbUrl, //variable de respaldo
+  },
 });
